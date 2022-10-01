85aa791128d08ec399e4cb102245a4a8c6b28529
emreceran <37599549+emreceran@users.noreply.github.com>
2022-10-01 11:16:24 +0300

    Update teskilat.py

--- models/teskilat.py
+++ models/teskilat.py
@@ -35,38 +35,38 @@ class karavanv1ilce(models.Model):
     state_id = fields.Many2one("res.country.state", string='State', ondelete='restrict',
                                related="sube_id.state_id")
 
-# class karavanokul(models.Model):
-#     _name = "karavan.okul"
-#     _description = "Okul"
-#     _order = 'name'
+class karavanokul(models.Model):
+    _name = "karavan.okul"
+    _description = "Okul"
+    _order = 'name'
 
-#     name = fields.Char(string="Okul İsim", store=True, readonly=False, tracking=True)
-#     user_id = fields.Many2one('res.users', 'User', related='resource_id.user_id', store=True, readonly=False)
+    name = fields.Char(string="Okul İsim", store=True, readonly=False, tracking=True)
+    user_id = fields.Many2one('res.users', 'User', related='resource_id.user_id', store=True, readonly=False)
 
-#     ilce_id = fields.Many2one('karavan.ilce', 'İlçe', store=True, readonly=False)
-#     sube_id = fields.Many2one("karavan.sube", string='Şube', store=True, ondelete='restrict',
-#                    related="ilce_id.sube_id")
+    ilce_id = fields.Many2one('karavan.ilce', 'İlçe', store=True, readonly=False)
+    sube_id = fields.Many2one("karavan.sube", string='Şube', store=True, ondelete='restrict',
+                   related="ilce_id.sube_id")
 
-# class karavantakim(models.Model):
-#     _name = "karavan.takim"
+class karavantakim(models.Model):
+    _name = "karavan.takim"
 
-#     _description = "karavan takim"
-#     _order = "name"
+    _description = "karavan takim"
+    _order = "name"
 
-#     # description
-#     name = fields.Char('Takım', required=True, translate=True)
-#     sequence = fields.Integer('Sequence', default=10)
-#     active = fields.Boolean(default=True,
-#                             help="If the active field is set to false, it will allow you to hide the Sales Team without removing it.")
-#     okul_id = fields.Many2one(
-#         'karavan.okul', string='Okul', index=True
-#     )
-#     ilce_id = fields.Many2one("karavan.ilce", string='İlçe', ondelete='restrict',
-#                               related="okul_id.ilce_id")
+    # description
+    name = fields.Char('Takım', required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=10)
+    active = fields.Boolean(default=True,
+                            help="If the active field is set to false, it will allow you to hide the Sales Team without removing it.")
+    okul_id = fields.Many2one(
+        'karavan.okul', string='Okul', index=True
+    )
+    ilce_id = fields.Many2one("karavan.ilce", string='İlçe', ondelete='restrict',
+                              related="okul_id.ilce_id")
 
-#     user_id = fields.Many2one('res.users', string='Kaptan')
+    user_id = fields.Many2one('res.users', string='Kaptan')
 
-#     member_ids = fields.Many2many(
-#         'res.users', string='Takım Üyeleri',
-#         domain="[ ('okul_id', '=', okul_id)]"
-#     )
+    member_ids = fields.Many2many(
+        'res.users', string='Takım Üyeleri',
+        domain="[ ('okul_id', '=', okul_id)]"
+    )
