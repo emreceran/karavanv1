be16a6ae919471d3bd842b9c4def3e62eb5a58c1
emreceran <37599549+emreceran@users.noreply.github.com>
2022-10-01 12:02:58 +0300

    Update res_users.py

--- models/res_users.py
+++ models/res_users.py
@@ -15,16 +15,19 @@ from odoo.release import version
 
 class karavanuser(models.Model):
     _inherit = "res.partner"
-
-    okul_id = fields.Many2one(
-        'karavan.okul', string='Okul', index=True, domain="[('ilce_id', '=?', ilce_id)]"
-    )
-
+    
+    sube_id = fields.Many2one("karavan.sube", string='Şube', ondelete='restrict',
+                                                            )
+    
     ilce_id = fields.Many2one("karavan.ilce", string='İlçe', ondelete='restrict',
                               # related="okul_id.ilce_id"
                               domain="[('sube_id', '=?', sube_id)]"
                               )
 
-    sube_id = fields.Many2one("karavan.sube", string='Şube', ondelete='restrict',
-                               # related="ilce_id.sube_id"
-                              )
+    okul_id = fields.Many2one(
+        'karavan.okul', string='Okul', index=True, domain="[('ilce_id', '=?', ilce_id)]"
+    )
+
+    
+
+    
